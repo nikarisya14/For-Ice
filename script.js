97a6b009b35e5905e203d81ca385c64f8655ac99
nikarisya14 <nikarisya14@gmail.com>
2026-07-22 14:03:13 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+let step = 0;
+
+function nextPage() {
+  let icon = document.getElementById("icon");
+  let title = document.getElementById("title");
+  let message = document.getElementById("message");
+  let btn = document.getElementById("nextBtn");
+  let music = document.getElementById("bgMusic");
+
+  // Mainkan muzik bila dia tekan "haii" buat pertama kali
+  if (step === 0) {
+    music.play().catch(e => console.log("Audio play blocked"));
+  }
+
+  step++;
+
+  // Bila tekan "haii", baru masuk babak seterusnya
+  if (step === 1) {
+    icon.innerText = "✨";
+    title.innerText = "Ada pesanan khas...";
+    message.innerText = "Saja nak hantar link ni kat kau 🫣";
+    btn.innerText = "Tekan Sini ➡️";
+  } 
+  else if (step === 2) {
+    icon.innerText = "🙈";
+    title.innerText = "Sebenarnya kan...";
+    message.innerText = "Kau selalu buat aku senyum sorang-sorang tahu tak?";
+    btn.innerText = "Lagi... 💖";
+  } 
+  else if (step === 3) {
+    icon.innerText = "💌";
+    title.innerText = "Thank You!";
+    message.innerText = "Terima kasih sebab sentiasa ada dan buat hari-hari aku lebih bermakna.";
+    btn.innerText = "Wish Khas ✨";
+  } 
+  else if (step === 4) {
+    icon.innerText = "🌸";
+    title.innerText = "Wish Untuk Kau";
+    message.innerText = "Semoga segala urusan kau sentiasa dipermudahkan dan bahagia selalu!";
+    btn.innerText = "Habis dah! ❤️";
+  } 
+  else {
+    icon.innerText = "💖";
+    title.innerText = "Hehe That's All!";
+    message.innerText = "Send screenshot ni kat aku kalau dah habis baca 😉";
+    btn.style.display = "none"; // Sembunyikan button bila dah tamat
+  }
+}
